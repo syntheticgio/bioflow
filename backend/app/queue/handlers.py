b044f5967027aadd30c4314d772c4aa04900b9a3
John Torcivia <jtorcivia@gmail.com>
2026-08-07 08:58:31 -0400

    feat: add DE summary queue handler

--- backend/app/queue/handlers.py
+++ backend/app/queue/handlers.py
@@ -878,6 +878,7 @@ from app.queue import (  # noqa: E402, F401
     align_handlers,
     assembly_handlers,
     assembly_qc_handlers,
+    de_summary_handlers,
     expression_handlers,
     lineage_handlers,
     ncbi_assembly_handlers,
